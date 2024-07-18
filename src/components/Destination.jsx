13640958f8bbcf2174f05544611baaed363e5003
Ying Tong <yingtong0106@gmail.com>
2024-07-18 16:13:58 +0800

    Load data into components

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,21 +1,34 @@
-import cappadocia_img from '../assets/Cappadocia.jpg'
+import CappadociaImg from '../assets/Cappadocia.jpg'
+import IstanbulImg from "../assets/Istanbul.jpg"
+import TaipeiImg from "../assets/Taipei.jpg"
+import HanoiImg from "../assets/Hanoi.jpg"
+
+const imagesMap = {
+    Cappadocia: CappadociaImg,
+    Istanbul: IstanbulImg,
+    Taipei: TaipeiImg,
+    Hanoi: HanoiImg
+}
+
 
 export default function Destination(props) {
+    const imgSrc = imagesMap[props.destination.locationName]
+
     return (
         <section className="destination">
-            <img src={cappadocia_img} 
-            alt="Cappadocia"
+            <img src={imgSrc} 
+            alt={props.destination.locationName}
             className='destination--img'
             />
             <div className="destination--details">
                 <div className="location--details">
                     <i className="fa-solid fa-location-dot"></i>
-                    <h4>TURKIYE</h4>
-                    <p><a href="https://google.com" target='_blank'>View on Google Maps</a></p>
+                    <h4>{props.destination.country.toUpperCase()}</h4>
+                    <p><a href={props.destination.googleMapsLink} target='_blank'>View on Google Maps</a></p>
                 </div>
-                <h1 className="destination--name">Cappadocia</h1>
-                <h3 className="destination--date">3 June, 2023 - 6 June, 2023</h3>
-                <p className="destination--description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, minus.</p>
+                <h1 className="destination--name">{props.destination.locationName}</h1>
+                <h3 className="destination--date">{props.destination.fromDate} - {props.destination.toDate}</h3>
+                <p className="destination--description">{props.destination.locationDescription}</p>
             </div>
         </section>
     )
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
     {
         id : 1,
-        location: "Turkiye",
+        country: "Turkiye",
         googleMapsLink: "https://www.google.com",
         locationName: 'Cappadocia',
         fromDate: "3 June, 2023",
@@ -10,11 +10,29 @@ export default [
     },
     {
         id: 2,
-        location: "Turkiye",
+        country: "Turkiye",
         googleMapsLink: "https://www.youtube.com",
         locationName: "Istanbul",
         fromDate: "26 May, 2023",
         toDate: "28 May, 2023",
         locationDescription: "Istanbul is cool af"
+    },
+    {
+        id: 3,
+        country: 'Taiwan',
+        googleMapsLink: "https://www.chatgpt.com",
+        locationName: "Taipei",
+        fromDate: "11 November, 2018",
+        toDate: "15 November, 2018",
+        locationDescription: "Nice food"
+    },
+    {
+        id: 4,
+        country: "Vietnam",
+        googleMapsLink: "https://www.wikipedia.com",
+        locationName: "Hanoi",
+        fromDate: "5 June, 2016",
+        toDate: "11 June, 2016",
+        locationDescription: "Pho for life"
     }
 ]
